cc5a3ea233b7466bfc96e6ce1c8d61f60472a2aa
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2020-08-13 18:16:38 -0700

    final edits

--- Develop/app.js
+++ Develop/app.js
@@ -4,19 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-//this array is for testing
-// const out = [];
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 //create output directory fs
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-// const Employee = require("./lib/Employee");
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
 // array of questions for user
 const questions = [
@@ -62,7 +53,6 @@ const questions = [
     type: "input",
     message: "What is your GitHub username?",
     when: function (answers) {
-      //   console.log(answers.role);
       if (answers.role.includes("Engineer")) {
         return true;
       }
@@ -73,7 +63,6 @@ const questions = [
     type: "input",
     message: "What School are you attending?",
     when: function (answers) {
-      //   console.log(answers.role);
       if (answers.role.includes("Intern")) {
         return true;
       }
@@ -84,7 +73,6 @@ const questions = [
     type: "input",
     message: "What is your Office Phone number?",
     when: function (answers) {
-      //   console.log(answers.role);
       if (answers.role.includes("Manager")) {
         return true;
       }
@@ -96,7 +84,6 @@ const questions = [
       if (pass) {
         return true;
       }
-
       return "Please enter a valid phone number";
     },
   },
@@ -120,9 +107,7 @@ function init(employees = []) {
           answers.school,
           answers.role
         );
-        //console.log(intern);
         employees.push(intern);
-        // console.log(employees);
       }
       if (answers.role.includes("Engineer")) {
         let engineer = new Engineer(
@@ -132,9 +117,7 @@ function init(employees = []) {
           answers.gitHub,
           answers.role
         );
-        //console.log(engineer);
         employees.push(engineer);
-        // console.log(employees);
       }
       if (answers.role.includes("Manager")) {
         let manager = new Manager(
@@ -144,22 +127,17 @@ function init(employees = []) {
           answers.phone,
           answers.role
         );
-        //console.log(manager);
         employees.push(manager);
-        console.log(employees);
       }
-      // fs.appendFile("manager.html", manager)
       if (answers.askAgain) {
         init(employees);
       } else {
-        //render function
         
-        console.log(employees);
-        fs.writeFileSync(outputPath, render(employees) + "\n", function (err) {
+        fs.writeFileSync(outputPath, render(employees), function (err) {
           if (err) {
             console.log(err);
           } else {
-            console.log("Commit logged!");
+            // console.log("Commit logged!");
           }
         });
         //end the CLI and push the information to proper documents
@@ -173,47 +151,5 @@ function init(employees = []) {
       }
     });
 }
-// function to write README file
-//   function writeToFile(answers) {
-//     const lic = answers.license;
-//     let licenseBDG = [];
-//     if (lic.includes("MIT License")) {
-//       licenseBDG.push(
-//         "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
-//       );
-//     }
-//     if (lic.includes("Apache License")) {
-//       licenseBDG.push(
-//         "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-//       );
-//     }
-//     if (lic.includes("GPL License")) {
-//       licenseBDG.push(
-//         "[![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)](http://perso.crans.org/besson/LICENSE.html)"
-//       );
-//     }
-//     if (lic.includes("NONE")) {
-//       licenseBDG.push("None");
-//     }
 
 init();
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
